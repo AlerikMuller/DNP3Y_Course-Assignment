5234ff80e28231cfc40c031d227c5c8ed68d6cf3
Alerik Muller <344325@via.dk>
2025-09-15 19:08:29 +0200

    Completed Draft Assignment 1

--- Entities/User.cs
+++ Entities/User.cs
@@ -2,5 +2,8 @@ namespace Entities;
 
 public class User
 {
-    
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int UserId { get; set; }
 }
