6edc898649e867f112de4546acd754a6ab60943c
Alerik Muller <344325@via.dk>
2025-09-23 21:11:16 +0200

    Completed Draft Assignment 3

---
+++ FileRepositories/PostFileRepository.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using Entities;
+using RepositoryContracts;
+
+namespace FileRepositories;
+
+public class PostFileRepository : IPostRepository
+{
+    private readonly string filePath = "posts.json";
+
+    public PostFileRepository()
+    {
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+    }
+
+    public async Task<Post> AddAsync(Post post)
+    {
+        string postsAsJson = await File.ReadAllTextAsync(filePath);
+        List<Post> posts = JsonSerializer.Deserialize<List<Post>>(postsAsJson);
+        int maxId = posts.Count > 0 ? posts.Max(p => p.Id) : 1;
+        post.Id = maxId + 1;
+        posts.Add(post);
+        postsAsJson = JsonSerializer.Serialize(posts);
+        await File.WriteAllTextAsync(filePath, postsAsJson);
+        return post;
+    }
+
+    public IQueryable<Post> GetMany()
+    {
+        string postsAsJson = File.ReadAllTextAsync(filePath).Result;
+        List<Post> posts = JsonSerializer.Deserialize<List<Post>>(PostsAsJson);
+        return posts.AsQueryable();
+    }
+}
---
+++ FileRepositories/UserFileRepository.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using Entities;
+using RepositoryContracts;
+
+namespace FileRepositories;
+
+public class UserFileRepository : IUserRepository
+{
+    private readonly string filePath = "users.json";
+
+    public UserFileRepository()
+    {
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+    }
+
+    public async Task<User> AddAsync(User user)
+    {
+        string usersAsJson = await File.ReadAllTextAsync(filePath);
+        List<User> users = JsonSerializer.Deserialize<List<User>>(usersAsJson);
+        int maxId = users.Count > 0 ? users.Max(u => u.Id) : 1;
+        user.Id = maxId + 1;
+        users.Add(user);
+        usersAsJson = JsonSerializer.Serialize(users);
+        await File.WriteAllTextAsync(filePath, usersAsJson);
+        return user;
+    }
+
+    public IQueryable<User> GetMany()
+    {
+        string usersAsJson = File.ReadAllTextAsync(filePath).Result;
+        List<User> users = JsonSerializer.Deserialize<List<User>>(usersAsJson);
+        return users.AsQueryable();
+    }
+}
